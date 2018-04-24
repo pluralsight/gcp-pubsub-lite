8cf8b17181a363b72a6b9eb75306d557027a520f
Charlie Dibble <cgdibble@gmail.com>
2018-04-23 18:32:10 -0700

    initial commit with skeleton to dev against for pubusb

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Test
+npm test
+if [[ $? -ne 0 ]] ; then
+  echo "Tests failed."
+  exit $?
+fi
+echo 'test complete'
+
+# Upgrade patch version
+npm version patch
+echo 'updated patch version'
+
+# Deploy to NPM
+npm publish --access public
+echo 'publish complete'
